9fc8b37eab94a66d69108790876061138071e3f4
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-04-18 09:17:18 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize (params[:pirate])
+    @name = params[:pirate][:name]
+    @weight = params[:pirate][:weight]
+    @height = params[:pirate][:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
